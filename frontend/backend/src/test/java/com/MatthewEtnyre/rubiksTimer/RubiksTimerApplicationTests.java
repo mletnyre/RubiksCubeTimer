fcac7590eca030ff51c8d7a04860ad085575daf9
Matthew Etnyre <mletnyre@iastate.edu>
2025-11-24 16:06:38 -0600

    removed redundant folder

---
+++ frontend/backend/src/test/java/com/MatthewEtnyre/rubiksTimer/RubiksTimerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MatthewEtnyre.rubiksTimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubiksTimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
